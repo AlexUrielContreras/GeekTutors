defa8ae729d1bb98239d811095b84224c78c943d
Alex Contreras <alex.code29@gmail.com>
2023-01-14 23:47:00 -0600

    refactor: fixed typo in connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,8 @@
-const mongooes = require('mongoose');
+const mongoose = require('mongoose');
 
-mongooes.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/geek-tutor', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/geek-tutor', {
    useNewUrlParser: true,
    useUnifiedTopology: true
-}).catch(
-   error => handleError(error)
-);
+})
 
-module.exports = mongooes.connection
+module.exports = mongoose.connection
