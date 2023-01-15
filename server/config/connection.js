6a201ae38a3c261d5534f4f4706407948ea149a3
Alex Contreras <alex.code29@gmail.com>
2023-01-14 23:54:55 -0600

    refactor: removed a connection option

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/geek-tutor', {
-   useNewUrlParser: true,
-   useUnifiedTopology: true
+   useNewUrlParser: true
 })
 
 module.exports = mongoose.connection
